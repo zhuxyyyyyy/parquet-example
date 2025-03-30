64f46b38170ef718bd633f6815745d749e3f45c4
zxy <zhuxythu18@gmail.com>
2025-03-30 16:50:25 +0800

    using colzip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,29 @@ package org.example;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Level;
 import org.apache.parquet.example.data.Group;
 import org.apache.parquet.example.data.simple.SimpleGroupFactory;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.example.GroupWriteSupport;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.MessageTypeParser;
+import org.apache.hadoop.fs.FileSystem;
 
 import org.apache.avro.generic.GenericRecord;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+
     public static void writeParquet() throws Exception {
         // 定义 Parquet 的 schema
         String schemaString = "message example { " +
                 "required binary name (UTF8); " +
-                "required int32 age; " +
                 "}";
         MessageType schema = MessageTypeParser.parseMessageType(schemaString);
 
@@ -31,6 +36,12 @@ public class Main {
         // 设置输出文件路径
         Path path = new Path("example.parquet");
 
+        // 如果存在，则删除
+        FileSystem fs = path.getFileSystem(conf);
+        if (fs.exists(path)) {
+            fs.delete(path, true); // 第二个参数 true 表示递归删除
+        }
+
         // 创建 ParquetWriter
         ParquetWriter<Group> writer = new ParquetWriter<>(
                 path,
@@ -38,14 +49,15 @@ public class Main {
                 new GroupWriteSupport()
         );
 
-        // 使用 SimpleGroupFactory 创建数据记录
-        SimpleGroupFactory groupFactory = new SimpleGroupFactory(schema);
-        Group group = groupFactory.newGroup()
-                .append("name", "Alice")
-                .append("age", 30);
 
-        // 写入记录并关闭 writer
-        writer.write(group);
+        for (int i = 0; i < 20; i++) {
+            SimpleGroupFactory groupFactory = new SimpleGroupFactory(schema);
+            String value = String.valueOf(i);
+            Group group = groupFactory.newGroup()
+                    .append("name", value);
+            writer.write(group);
+        }
+
         writer.close();
     }
 
@@ -63,6 +75,11 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        log.setLevel(Level.INFO);
+        log.info("Starting parquet file system");
+
+        writeParquet();
         readParquet();
     }
 }
